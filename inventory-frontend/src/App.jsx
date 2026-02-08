7421aece5b9943718370b53b734272503231634f
JoeB1d0me <96071431+JoeB1d0me@users.noreply.github.com>
2026-02-07 19:39:13 -0500

    Built prototype template with reactjs

--- inventory-frontend/src/App.jsx
+++ inventory-frontend/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://127.0.0.1:5000/api/inventory")
+      .then(res => res.json())
+      .then(data => setInventory(data));
+
+  }, []);
+
+  
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+
+      <h1>📊 Inventory Health Monitor</h1>
+
+      <table>
+
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Stock</th>
+            <th>Avg Sales</th>
+            <th>Days Left</th>
+            <th>Reorder Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          {inventory.map(item => (
+
+            <tr key={item.id}>
+
+              <td>{item.item}</td>
+              <td>{item.stock}</td>
+              <td>{item.avg_sales}</td>
+              <td>{item.days_left}</td>
+              <td>{item.reorder_date}</td>
+
+              <td>
+
+                {item.status === "CRITICAL" &&
+                  <span className="critical">CRITICAL</span>}
+
+                {item.status === "LOW" &&
+                  <span className="low">LOW</span>}
+
+                {item.status === "OK" &&
+                  <span className="ok">OK</span>}
+
+              </td>
+
+            </tr>
+
+          ))}
+
+        </tbody>
+
+      </table>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
